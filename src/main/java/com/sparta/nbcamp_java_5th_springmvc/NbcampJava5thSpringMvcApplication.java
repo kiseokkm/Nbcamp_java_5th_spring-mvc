2ad0053f606fdb67aa1b8f7da6d762769c271b8a
kiseokkm <311kskim@gmail.com>
2024-05-14 11:27:01 +0900

    Feat : set

---
+++ src/main/java/com/sparta/nbcamp_java_5th_springmvc/NbcampJava5thSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcamp_java_5th_springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcampJava5thSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcampJava5thSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/nbcamp_java_5th_springmvc/NbcampJava5thSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcamp_java_5th_springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcampJava5thSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
